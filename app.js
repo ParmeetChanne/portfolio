3778f9f5da7efe7bceefe0f7e5d6c2602c48cfe7
Parmeet Channe <parmeetchanne@gmail.com>
2021-06-05 19:36:37 +0530

    animations

--- app.js
+++ app.js
@@ -39,3 +39,29 @@ Number.prototype.pad = function (digits) {
 
 document.getElementById("datee").innerHTML = months[d.getMonth()];
 document.getElementById("month").innerHTML = d.getDate().pad(2);
+
+var textWrapper = document.querySelector(".header .header_1");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".header .header_1",
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i,
+  })
+  .add({
+    targets: ".header",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
